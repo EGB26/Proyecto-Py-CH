7f947ddcafb9bebefea86bd26641cbba9e4719b9
Emmanuel Giudice <profesorgiudice@gmail.com>
2023-02-13 15:30:36 -0300

    codigo funcionando - herencia template y admin

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+#from AppCoder.models import *
 
 # Create your views here.
+def inicio(request):
+    return render(request, 'AppCoder/inicio.html')
+    #return HttpResponse('vista inicio')
+
+def cursos(request):
+    return render(request, 'AppCoder/cursos.html')
+    #return HttpResponse('vista cursos')
+
+def profesores(request):
+    return render(request, 'AppCoder/profesores.html')
+    #return HttpResponse('vista profesores')
+
+def estudiantes(request):
+    return render(request, 'AppCoder/estudiantes.html')
+    #return HttpResponse('vista estudiantes')
+
+def entregables(request):
+    return render(request, 'AppCoder/entregables.html')
+    #return HttpResponse('vista entregables')
+""""
 def curso(self):
-    Curso = curso(nombre="Desarrollo Web", camada=19881)
-    Curso.save()
-    documentoDeTexto = f"--->Curso: {Curso.nombre} Camada: {Curso.camada}"
+    curso = Curso(nombre="Desarrollo Web", camada=19881)
+    curso.save()
+    documentoDeTexto = f"--->Curso: {curso.nombre} Camada: {curso.camada}"
     return HttpResponse(documentoDeTexto)
+"""
